a465df51e1c6551abe3e5e4397ebb130db8bd2ac
natmacira <natimciraolo@gmail.com>
2022-05-17 16:00:15 -0300

    primer tarea llamar archivos

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+const asanas = require  ("./asanas");
+
 app.get('/', (req, res)=> {
-    res.send('Hola, mundo, acá probando el nodemon!! a ver si sigue funcionando');
+    res.send('Hola, mundo, acá probando el nodemon!! a ver si sigue funcionando....y vienen las asanas:  ' +  asanas);
+    console.log(asanas);
+ 
 });
 
 app.listen(PORT, () => console.log("Ejecutando aplicación..."));
---
+++ asanas.js
@@ -0,0 +1,5 @@
+
+
+const asanas = ["vriksana", "parvatasana", "setubandasana", "shavasana"];
+
+module.exports = asanas;
